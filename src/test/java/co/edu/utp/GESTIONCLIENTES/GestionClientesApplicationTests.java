cbc6fa6e22a7a856cd35bb36a4417c7cc90bf7cb
CamiloSepulvedaUTP <prosof.camilo.sepulveda@gmail.com>
2020-05-10 18:13:50 -0500

    Inicializando Proyecto

---
+++ src/test/java/co/edu/utp/GESTIONCLIENTES/GestionClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.utp.GESTIONCLIENTES;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
